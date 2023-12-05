c4a6b718a5a72539468ca01903901d5cef2ce528
SkylerRo <149194025+SkylerRo@users.noreply.github.com>
2023-12-04 20:01:25 -0500

    first commit

--- index.js
+++ index.js
@@ -1,122 +1,3 @@
-// The provided course information.
-const CourseInfo = {
-    id: 451,
-    name: "Introduction to JavaScript"
-  };
-  
-  // The provided assignment group.
-  const AssignmentGroup = {
-    id: 12345,
-    name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
-    assignments: [
-      {
-        id: 1,
-        name: "Declare a Variable",
-        due_at: "2023-01-25",
-        points_possible: 50
-      },
-      {
-        id: 2,
-        name: "Write a Function",
-        due_at: "2023-02-27",
-        points_possible: 150
-      },
-      {
-        id: 3,
-        name: "Code the World",
-        due_at: "3156-11-15",
-        points_possible: 500
-      }
-    ]
-  };
-  
-  // The provided learner submission data.
-  const LearnerSubmissions = [
-    {
-      learner_id: 125,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 47
-      }
-    },
-    {
-      learner_id: 125,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-02-12",
-        score: 150
-      }
-    },
-    {
-      learner_id: 125,
-      assignment_id: 3,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 400
-      }
-    },
-    {
-      learner_id: 132,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-24",
-        score: 39
-      }
-    },
-    {
-      learner_id: 132,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-03-07",
-        score: 140
-      }
-    }
-  ];
-   
-  let uniqueId = [...new Set(LearnerSubmissions.map(item=>item.learner_id))]
-  console.log(uniqueId)
-
-  let assignmentScore = [...new Set(LearnerSubmissions.map(item=>item.submission.score))]
-  console.log (assignmentScore
-    
-    )
-  function getLearnerData(course, ag, submissions) {
-    // here, we would process this data to achieve the desired result.
-    // const result = [
-    //   {
-    //     id: 125,
-    //     avg: 0.985, // (47 + 150) / (50 + 150)
-    //     1: 0.94, // 47 / 50
-    //     2: 1.0 // 150 / 150
-    //   },
-    //   {
-    //     id: 132,
-    //     avg: 0.82, // (39 + 125) / (50 + 150)
-    //     1: 0.78, // 39 / 50
-    //     2: 0.833 // late: (140 - 15) / 150
-    //   }
-    // ];
-  let results = []
-  for ( let i = 0; i <= uniqueId.length-1; i++){
-    results.push ({learner_id:uniqueId[i]})
-  }
-  for (let i of LearnerSubmissions) {
-    if (LearnerSubmissions.learner_id == results.learner_id){
-        results.push(LearnerSubmissions[i].submissions.score)
-    }
-  }
-//   results.push ({uniqueId})
-  console.log(results)
-    return results;
-  }
-  
-  const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-  
-  console.log(result);
- 
 
 
   // The provided course information.
@@ -222,11 +103,14 @@ const CourseInfo = {
   // 190/200 on another would have a weighted average score of 240/300 = 80%.
   
   
-  // Create a function named getLearnerData() that accepts these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission])
+  // Create a function named getLearnerData() that accepts these values as parameters, 
+  //in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission])
   
   function getLearnerData(course, ag, submissions) {
   
-    // If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
+    // If an AssignmentGroup does not belong to its course (mismatching course_id), 
+    //your program should throw an error, letting the user know that the input was invalid. 
+    //Similar data validation should occur elsewhere within the program.
   
     if (ag.course_id !== course.id) {
       throw new Error("Invalid input: Assignment group doesn't match course id.");
@@ -271,8 +155,21 @@ const CourseInfo = {
       if (assignment) {
   
         // similar process to calculateWeightedAverage, calculating for individual learners:
-        // checks if the submission is late by comparing the submission's date to the assignment's due date.
-  
+
+        //check if submssion is due
+        
+       /*  let IsDue;
+        if (submission.submission.submitted_at ==  assignmentScore.due_at) {
+          isDue = true
+        } else {
+          isDue = false
+        } */
+
+          
+        const isDue = submission.submission.submitted_at == assignment.due_at;
+
+        // checks if the submission is late by comparing the submission's date to the assignment's due date
+
         const lateSubmission = new Date(submission.submission.submitted_at) > new Date(assignment.due_at);
   
         // points_possible must be at least 1 to avoid division by zero
@@ -293,11 +190,11 @@ const CourseInfo = {
         // calculating the weighted score for the assignment
   
         learner.totalScore += (submission.submission.score / pointsPossible) * latePoints;
-  
+          // 2.74
         // adds the late points to total possible points
   
         learner.totalPossible += latePoints;
-  
+            //700
         // saves percentage score ot learner 'scores' of the assignment if not late
   
         if (!lateSubmission) {
@@ -475,3 +372,4 @@ const CourseInfo = {
   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
   
   console.log(result);
+
